121aaffbc0b44384ce584e35db4fa3627fdef7fd
LuisToro-Desarrollo <luistorodeveloment@gmail.com>
2022-09-11 11:19:43 -0500

    mostranso en pantalla

---
+++ assets/js/alerts.js
@@ -0,0 +1,5 @@
+alert('hola mund0');
+let nombre =prompt('¿cual es tu nombre?', 'Ingresa tu nombre');
+console.log('hola'+ nombre+'chao');
+let sel= confirm('esta seguro de eliminar el archivo');
+console.log(sel);
