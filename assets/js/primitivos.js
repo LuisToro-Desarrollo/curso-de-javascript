d487234caa80502a9a102b8ae3b28aba491712e9
LuisToro-Desarrollo <luistorodeveloment@gmail.com>
2022-09-11 12:22:21 -0500

    datos primitivos

---
+++ assets/js/primitivos.js
@@ -0,0 +1,21 @@
+let nombre = "luis toro acosta";
+console.log(nombre);
+
+nombre = "arturo diaz";
+console.log(nombre);
+
+console.log(typeof nombre);
+
+nombre = 1234;
+
+console.log(typeof nombre);
+let esmarvel = true;
+console.log(esmarvel);
+console.log(typeof esmarvel);
+let edad =34;
+console.log(edad);
+console.log(typeof edad);
+let symbol1= Symbol('a');
+console.log(typeof symbol1);
+let symbol2= Symbol('a');
+console.log(typeof symbol2);
