9e0422786d492206f757c28dc4309fca56d5cc22
LuisToro-Desarrollo <luistorodeveloment@gmail.com>
2022-09-11 15:07:41 -0500

    conociendo palabras reservadas

---
+++ assets/js/palabras-reservadas.js
@@ -0,0 +1,9 @@
+console.log('hollla');
+
+let numero= prompt('indique su edad', 'ingrese su edad');
+
+if (numero<18){
+    console.log('es menor de edad');
+}else {
+    console.log('es mayor de edad');
+}
