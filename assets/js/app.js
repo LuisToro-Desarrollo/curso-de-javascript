01a9e8cc2ef106b8921237a6070f40a7d68e277c
LuisToro-Desarrollo <luistorodeveloment@gmail.com>
2022-09-11 10:29:06 -0500

    depurar-variables

---
+++ assets/js/app.js
@@ -0,0 +1,25 @@
+
+
+//console.log ('hola mundo');
+//variables
+let a= 10;
+let b= 20;
+let c= 30;
+
+let suma = a +b;
+//console.log(suma);
+
+let total=suma+c;
+//console.log(total);
+
+//alert('hola desde consola');
+
+var minombre = 'Pedro Luis Toro acosta';
+
+
+
+
+
+
+
+
