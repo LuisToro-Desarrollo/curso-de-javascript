20bd9beb8355a8b6713a1498c148088bf860c8cd
LuisToro-Desarrollo <luistorodeveloment@gmail.com>
2022-09-11 20:17:27 -0500

    funciones en javascript

---
+++ assets/js/funciones.js
@@ -0,0 +1,15 @@
+function saludar () {
+ console.log ('hola mundo');
+};
+
+saludar();
+saludar();
+
+let nombre= prompt('Por favor ingrese su nombre','ingrese su nombre')
+function saludar2(nombre){
+console.log('Hola' + nombre);
+}
+saludar2(nombre);
+saludar2('karlos');
+saludar2('maria');
+saludar2('ferde');
