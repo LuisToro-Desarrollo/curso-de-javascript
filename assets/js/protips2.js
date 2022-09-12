3d3b8da5f6a07b1bfe599742846bd13f26433fcf
Pedro Luis Toro Acosta <pedro.toro@OnOffcol.loc>
2022-09-12 13:47:48 -0500

    desestructuracion parte 1

---
+++ assets/js/protips2.js
@@ -0,0 +1,55 @@
+const tony = {
+    nombre: 'Tony Stack',
+    CodeName: 'Ironman',
+    vivo: false,
+    edad:40,
+    trajes: ['Mark I', 'Mark V', 'Hulkbuster'],
+    
+};
+
+/*const imprimePropiedades = (personaje ) => {
+    console.log(personaje.nombre);
+    console.log(personaje.CodeName);
+    console.log(personaje.vivo);
+    console.log(personaje.edad);
+    console.log(personaje.trajes);
+
+}
+
+imprimePropiedades( tony );
+
+**/
+
+const imprimePropiedades = (personaje ) => {
+    console.log(personaje.nombre);
+    console.log(personaje.CodeName);
+    console.log(personaje.vivo);
+    console.log(personaje.edad);
+    console.log(personaje.trajes);
+
+}
+
+imprimePropiedades( tony );
+
+console.log('mi nombre');
+
+let edad =prompt ('por favor indique su edad','ingrese su edad');
+
+if (edad<18) {
+    alert('es menor de edad');
+    {
+        location.href ="http://www.pagina1.com";
+        }
+}
+
+else {
+    alert('es mayor de edad');
+}
+
+//ejemplo con if ternario
+
+ edad <18 ? (console.log('es menor de edad'
+ )): (console.log('es mayor de edad'));
+
+
+
