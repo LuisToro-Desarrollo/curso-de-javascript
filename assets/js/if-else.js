b294a5e521c4d5d15a81d83df7d5bfa9b35f4ed9
Pedro Luis Toro Acosta <pedro.toro@OnOffcol.loc>
2022-09-12 15:52:03 -0500

    repasando if

---
+++ assets/js/if-else.js
@@ -0,0 +1,27 @@
+let a = 9;
+
+if (a>=10){
+
+    console.log ('A es mayor o igual a 10');
+
+} else {
+
+    console.log ('es menor  a 10');
+}
+
+const hoy = new Date();
+let dia = hoy.getDay();
+
+if(dia === 1){
+    console.log('lunes');
+}
+else if (dia === 2) {
+    console.log('Martes');
+} 
+else if (dia === 3){
+    console.log('miercoles');
+} else if (dia === 4){
+console.log('jueves');
+}
+
+
