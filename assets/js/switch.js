a6bbc6788f749810dafbc32856541ea76314f2b5
Pedro Luis Toro Acosta <pedro.toro@OnOffcol.loc>
2022-09-13 10:31:31 -0500

    switch

---
+++ assets/js/switch.js
@@ -0,0 +1,19 @@
+const dia = 2; 
+
+switch (dia){
+ case 0 : console.log('Domingo');
+ break;
+ case 1 : console.log('Lunes');
+ break;
+ case 2 : console.log('Martes');
+ break;
+ case 3 : console.log('Miercoles');
+ break;
+ case 4 : console.log('Jueves');
+ break;
+ case 5 : console.log('viernes');
+ break;
+ case  6: console.log('sabado');
+ break;
+default: console.log('Opcion incorrecta');
+}
