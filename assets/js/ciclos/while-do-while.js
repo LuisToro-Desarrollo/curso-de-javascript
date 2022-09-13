2aa325dc74c3dfe5ea026d1fb6028f23f6fafeb4
Pedro Luis Toro Acosta <pedro.toro@OnOffcol.loc>
2022-09-13 11:59:15 -0500

    ciclo while y do while

---
+++ assets/js/ciclos/while-do-while.js
@@ -0,0 +1,41 @@
+console.log('hola');
+
+const carros = [
+    'ford',
+    'Mazda',
+    'Honda',
+    'Toyota',
+];
+let i= 0;
+while(i<carros.length){
+
+    console.log(carros[i]);
+    i++;
+}
+
+// otro ejemplo
+
+let x = 0;
+let Bravos= false;
+
+
+let perro =[
+'doberman', 
+'pincher',
+ (Bravos) ? 'Pitbull' : 'asa de te'
+
+];
+
+
+while (x < perro.length){
+    console.log(perro[x])
+    x++;
+}
+
+x=0;
+console.log('------------------------------');
+do{
+    console.log(perro[x])
+    x++;
+
+}while(x<perro.length);
